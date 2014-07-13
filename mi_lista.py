ba7c1e251a0a8fb6d2e063de6ad1b2c331f1637d
EduardoRL <ingteceduardo@gmail.com>
2014-07-13 23:40:22 +0200

    He actualizado el archivo de python de tuplas, listas y diccionarios

--- mi_lista.py
+++ mi_lista.py
@@ -1,1 +1,7 @@
-mi_lista = ("eduardo",222,111)
+mi_tupla = ("eduardo",222,111) # inmutable, no se puede modificar.
+mi_lista = ["eduardo",222,111] # si es modicable sus elementos.
+mi_diccionario = {"clave1":"eduardo","clave2":222,
+                  "clave3":111} # es modificable y se puede acceder a
+                                # sus elementos por una clave.
+
+
